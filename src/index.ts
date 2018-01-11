cd5a1f327e89009f62430f52ae454600997eb278
Bruno Carvalho <brunocarvalho107@gmail.com>
2018-01-11 20:12:18 -0200

    Changing the exports files to standardize the module

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-
-import { ViacepService } from './viacep.service';
-
-@NgModule({
-  imports: [
-    CommonModule,
-    HttpClientModule
-  ]
-})
-export class ViacepModule {
-
-  static forRoot(): ModuleWithProviders {
-
-    return {
-      ngModule: ViacepModule,
-      providers: [ViacepService]
-    };
-  }
-}
+export * from './viacep.module';
+export * from './viacep.service';
+export * from './endereco';

777563deb95da2d68affa0ef879e0a3bfcd4e21a
Bruno Carvalho <brunocarvalho107@gmail.com>
2018-07-29 17:02:35 -0300

    Experimenting with new build configs

--- .circleci/scripts/sync-version.sh
+++ .circleci/scripts/sync-version.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-echo "loadingthe environment"
-bash ~/repo/.circleci/scripts/get-last-tag.sh
-
 echo "cd into the ~/repo/projects/ngx-viacep/ folder"
 cd ~/repo/projects/ngx-viacep/
 
-last_tag=$TAG
-
-echo "setting the package version to $last_tag"
-npm --no-git-tag-version version ${last_tag}
+echo "setting the package version to $FULL_TAG"
+npm --no-git-tag-version version ${FULL_TAG}

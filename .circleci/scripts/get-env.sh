d507ed82e2c8e46cb370289b5dc17890637647a6
Bruno Carvalho <brunocarvalho107@gmail.com>
2018-07-29 17:56:45 -0300

    Adding the next tag to alpha and beta packages

--- .circleci/scripts/get-env.sh
+++ .circleci/scripts/get-env.sh
@@ -8,10 +8,13 @@ echo "Current branch: ${BRANCH}"
 
 if [[ ${CIRCLE_BRANCH} == "develop" ]]; then
     export FULL_TAG="${TAG}-alpha";
+    export NEXT="--tag next"
 elif [[ ${CIRCLE_BRANCH} == "staging" ]]; then
     export FULL_TAG="${TAG}-beta";
+    export NEXT="--tag next"
 elif [[ ${CIRCLE_BRANCH} == "master" ]]; then
     export FULL_TAG="${TAG}";
+    export NEXT=""
 fi
 
 

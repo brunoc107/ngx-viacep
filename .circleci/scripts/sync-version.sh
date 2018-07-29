070dd97045d60646ddd7abf4a2e374fbe17f8363
Bruno Carvalho <brunocarvalho107@gmail.com>
2018-07-29 15:00:42 -0300

    Improving the build logging

--- .circleci/scripts/sync-version.sh
+++ .circleci/scripts/sync-version.sh
@@ -2,7 +2,6 @@
 
 echo "cd into the ~/repo/projects/ngx-viacep/ folder"
 cd ~/repo/projects/ngx-viacep/
-pwd
 
 echo "setting the package version to $CIRCLE_TAG"
-npm version $CIRLCE_TAG
+npm version "$CIRLCE_TAG"

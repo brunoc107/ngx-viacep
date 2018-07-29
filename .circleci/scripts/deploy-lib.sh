2529aabb49e30b617caf35f6b76fd6754b2d9662
Bruno Carvalho <brunocarvalho107@gmail.com>
2018-07-29 11:44:05 -0300

    Updating the npm auth

--- .circleci/scripts/deploy-lib.sh
+++ .circleci/scripts/deploy-lib.sh
@@ -2,4 +2,6 @@
 
 cd ~/repo/dist/ngx-viacep
 
+echo "//registry.npmjs.org/:_authToken=$npm_token" >> ~/.npmrc
+
 npm publish
--- .circleci/scripts/npm-login.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "//registry.npmjs.org/:_authToken=$npm_token" > ~/repo/.npmrc
-
-echo $npm_token

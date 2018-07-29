b758af3addef061c7e2a76846638bb892c8ee455
Bruno Carvalho <brunocarvalho107@gmail.com>
2018-07-29 11:22:06 -0300

    Adding the CI deploy scripts

---
+++ .circleci/scripts/deploy-lib.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd ~/repo/dist/ngx-viacep/dist/ngx-viacep
+
+npm publish
---
+++ .circleci/scripts/npm-login.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "//registry.npmjs.org/:_authToken=$npm_token" > ~/repo/.npmrc

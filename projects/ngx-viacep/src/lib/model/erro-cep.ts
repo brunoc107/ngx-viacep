da0ab0efff96234b0569844aa048ab9960e8634e
Bruno Carvalho <brunocarvalho107@gmail.com>
2018-08-11 20:36:16 -0300

    Updating the ErroCep class to use the ErrorValues enum and removing the local enum declaration

--- projects/ngx-viacep/src/lib/model/erro-cep.ts
+++ projects/ngx-viacep/src/lib/model/erro-cep.ts
@@ -1,24 +1,24 @@
-enum ErroCepValues {
-  CEP_NAO_ENCONTRADO,
-  CEP_VAZIO,
-  CEP_INVALIDO,
-  CEP_MUITO_CURTO,
-  CEP_MUITO_LONGO,
-  ERRO_UF,
-  ERRO_UF_MUITO_CURTA,
-  ERRO_UF_MUITO_LONGA,
-  ERRO_UF_NAO_EXISTE,
-  ERRO_MUNICIPIO,
-  ERRO_MUNICIPIO_MUITO_CURTO,
-  ERRO_LOGRADOURO,
-  ERRO_LOGRADOURO_MUITO_CURTO,
-  ERRO_SERVIDOR
-}
+import {ErrorValues} from './error-values.enum';
 
 export class ErroCep extends Error {
 
-  constructor( message: Extract<keyof typeof ErroCepValues, string> ) {
+  constructor( private errorCode: ErrorValues ) {
 
-    super(message);
+    super(ErrorValues[errorCode]);
+  }
+
+  /**
+   * Returns the Error code
+   */
+  getCode(): ErrorValues {
+    return this.errorCode;
+  }
+
+  /**
+   * Compares the thrown error type with an ErrorValues enum item
+   * @param type
+   */
+  ofType(type: ErrorValues): boolean {
+    return this.getCode() === type;
   }
 }

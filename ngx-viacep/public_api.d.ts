69ebaa1c84e39dbf307dc838ab722e2129ccab93
Bruno Carvalho <brunocarvalho107@gmail.com>
2018-08-12 17:22:57 -0300

    Auto-generated commit

--- ngx-viacep/public_api.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/ngx-viacep.module';
-export * from './lib/ngx-viacep.service';
-export * from './lib/model/endereco';
-export * from './lib/model/error-values.enum';
-export * from './lib/model/erro-cep';

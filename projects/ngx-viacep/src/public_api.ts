6b4dad6b2c960fb4d65782b651e63211eed34c2d
Bruno Carvalho <brunocarvalho107@gmail.com>
2018-08-11 22:07:24 -0300

    Re-ordering the exports in the public API

--- projects/ngx-viacep/src/public_api.ts
+++ projects/ngx-viacep/src/public_api.ts
@@ -2,8 +2,8 @@
  * Public API Surface of ngx-viacep
  */
 
-export * from './lib/ngx-viacep.service';
 export * from './lib/ngx-viacep.module';
+export * from './lib/ngx-viacep.service';
 export * from './lib/model/endereco';
-export * from './lib/model/erro-cep';
 export * from './lib/model/error-values.enum';
+export * from './lib/model/erro-cep';

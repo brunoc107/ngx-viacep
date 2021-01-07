320a7208628799fa85044b2fa5488adb6a75610b
Bruno Evangelista <bruno.evangelista@maxmilhas.com.br>
2021-01-07 19:52:34 -0300

    Github actions first commit

--- .circleci/scripts/build-lib.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "copying the readme and license to the project folder"
-cp ~/repo/README.md ~/repo/projects/ngx-viacep/
-cp ~/repo/LICENSE ~/repo/projects/ngx-viacep/
-
-echo "building the lib"
-npm run build:lib:prod

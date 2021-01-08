2e8db86cdda982335ac7314d973b1442e4db37b7
Bruno Evangelista <bruno.evangelista@maxmilhas.com.br>
2021-01-07 22:51:53 -0300

    Updating the worflow

--- .github/workflows/scripts/publish.sh
+++ .github/workflows/scripts/publish.sh
@@ -14,9 +14,6 @@ if [ -z "$NPM_AUTH_TOKEN" ]; then
     echo "EMPTY_TOKEN_ERROR"
     exit 1
 fi
-echo _auth=$NPM_AUTH_TOKEN >> ./dist/ngx-viacep/.npmrc
-
-cat ./dist/ngx-viacep/.npmrc
 
 echo "Publishing the package"
 cd ./dist/ngx-viacep/

555ddfec56c111e635516a53513ae97080c07881
Bruno Evangelista <bruno.evangelista@maxmilhas.com.br>
2021-01-07 21:52:23 -0300

    Updating the worflow

---
+++ .github/workflows/scripts/copy-files.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Copying the lib package.json"
+cp projects/ngx-viacep/package.json ./dist/ngx-viacep/
+
+echo "Copying the lib README.md"
+cp ./README.md ./dist/ngx-viacep/
+
+echo "Copying the license"
+cp ./LICENSE ./dist/ngx-viacep/
+
+echo "Retrieving credentials"
+echo _auth=$NPM_AUTH_TOKEN >> ./dist/ngx-viacep/.npmrc
---
+++ .github/workflows/scripts/publish.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "Publishing the package"
+cd ./dist/ngx-viacep/
+npm publish 

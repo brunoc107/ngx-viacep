7d5a7abb1f4b4568070cb1399b851bdba084c73e
Bruno Evangelista <bruno.evangelista@maxmilhas.com.br>
2021-01-07 22:39:35 -0300

    Updating the worflow

--- .github/workflows/scripts/copy-files.sh
+++ .github/workflows/scripts/copy-files.sh
@@ -8,10 +8,3 @@ cp ./README.md ./dist/ngx-viacep/
 
 echo "Copying the license"
 cp ./LICENSE ./dist/ngx-viacep/
-
-echo "Retrieving credentials"
-if [ -z "$NPM_AUTH_TOKEN" ]; then
-    echo "EMPTY_TOKEN_ERROR"
-    exit 1
-fi
-echo _auth=$NPM_AUTH_TOKEN >> ./dist/ngx-viacep/.npmrc
--- .github/workflows/scripts/publish.sh
+++ .github/workflows/scripts/publish.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Retrieving credentials"
+if [ -z "$NPM_AUTH_TOKEN" ]; then
+    echo "EMPTY_TOKEN_ERROR"
+    exit 1
+fi
+echo _auth=$NPM_AUTH_TOKEN >> ./dist/ngx-viacep/.npmrc
+
 echo "Publishing the package"
 cd ./dist/ngx-viacep/
 npm publish 

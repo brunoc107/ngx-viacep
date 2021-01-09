5253018ad772ca9cc3d3eaef4aa9c9012f6a1cd1
Bruno Evangelista <bruno.evangelista@maxmilhas.com.br>
2021-01-09 12:25:38 -0300

    Adding a script to sync versions

--- .github/workflows/scripts/publish.sh
+++ .github/workflows/scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Updating the version"
-node ./sync-version.js
+node /home/runner/work/ngx-viacep/ngx-viacep/sync-version.js
 
 echo "Copying the lib package.json"
 cp projects/ngx-viacep/package.json ./dist/ngx-viacep/

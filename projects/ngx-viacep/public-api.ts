b6d23547807be70ca0eff7ed2bbeb7dc027f97b8
Bruno Evangelista <bruno.evangelista@maxmilhas.com.br>
2021-01-14 11:18:37 -0300

    Updating build

---
+++ projects/ngx-viacep/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of ngx-viacep
+ */
+
+export * from './src/lib/ngx-viacep.module';
+export * from './src/lib/ngx-viacep.service';
+export * from './src/lib/model/endereco';
+export * from './src/lib/model/cep-error-code';
+export * from './src/lib/model/cep-error';

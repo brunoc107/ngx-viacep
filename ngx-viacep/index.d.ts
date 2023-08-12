5fd1152881c54252234090c19f22763db39c38a3
Bruno Carvalho <brunocarvalho107@gmail.com>
2023-08-11 21:18:58 -0300

    Auto-generated commit [ci skip]

--- ngx-viacep/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './public-api';
-//# sourceMappingURL=index.d.ts.map
--- ngx-viacep/lib/model/constantes.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const BASE_URL = "https://viacep.com.br/ws";
-export declare const UFS_VALIDAS: string[];
-//# sourceMappingURL=constantes.d.ts.map
